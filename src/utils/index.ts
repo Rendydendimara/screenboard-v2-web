6d00291808d2975a06f19ada39e4846b8ad07049
“Rendy <r3ndydinar@gmail.com>
2025-11-10 18:54:01 +0700

    chore: reset getImageUrl base link

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,8 @@
 export function getImageUrl(link: string) {
   if (!link) return "/assets/image/user-placeholder.png";
-  return `http://66.42.52.110:8081${link}`;
+  return `${import.meta.env.VITE_API_BASE_URL}${link}`;
 }
+
 export function isValidImageSrc(src: string): boolean {
   return /^(\/|https?:\/\/)/.test(src);
 }
